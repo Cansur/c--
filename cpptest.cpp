f5e21d908fe5a6d58de593b83ac2d91f390ab940
Cansur <tjswo0682@gmail.com>
2024-03-10 16:52:27 +0900

    1

--- cpptest.cpp
+++ cpptest.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-#include <optional>
+#include <initializer_list>
+#include <cstdlib>
 
-int main()
-{
-    std::cout << "TEST" << std::endl;
-    std::optional<std::string> a("HI");
-    std::cout << a.value() << std::endl;
-    return 0;
+using namespace std;
+char list_exam(initializer_list<char> v, char p){
+    int num = 0;
+    for (auto val : v){
+        if (abs(static_cast<int>(val)-static_cast<int>(p)) < abs(num-static_cast<int>(p))){
+            num = static_cast<int>(val);
+        }
+    }
+    return static_cast<char>(num);
+}
+int main(){
+    cout << "{ 'd', 'p', 'r', 'w', 'g', 'f' }문자 중 h와 가까운 문자는 : " ;
+    cout << list_exam({ 'd', 'p', 'r', 'w', 'g', 'f' }, 'h') <<endl;
+    cout << "{ 'k', 'q', 'b', 'r', 'a', 'e', 'v', 'z'}문자 중 w와 가까운 문자는 : ";
+    cout << list_exam({ 'k', 'q', 'b', 'r', 'a', 'e', 'v', 'z'}, 'w') << endl;
 }
